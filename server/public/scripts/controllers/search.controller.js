fd39aa402d9ee450c493e431d73667d415b3b965
Darian N <darian.nasrabadi@gmail.com>
2018-01-29 00:55:07 -0600

    hard coded page but able to display data to the DOM based on selected drop down pick

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,6 +1,14 @@
 app.controller('SearchController', ['StarwarsService', function(StarwarsService) {
     const self = this;
+    self.starWarsResource = StarwarsService.starWarsResource
 
+    self.resourceSelected = function (resource) {
+        self.hideBorder = true
+        StarwarsService.resourceSelected(resource)
+        console.log(StarwarsService.starWarsResource);
+        
+    }
 
+    self.resourceArray = StarwarsService.resourceArray
 
 }]);
--- server/public/scripts/service/starwars.service.js
+++ server/public/scripts/service/starwars.service.js
@@ -1,13 +1,35 @@
 app.service('StarwarsService',['$http', function($http) {
 console.log('Service loaded');
     const self = this;
-
+    self.resourceArray = {list: []};
+    self.starWarsResource = {list: []};
 /* GET REQUESTS */
 
+    self.resourceSelected = function (resource) {
+        $http({
+            method: 'GET',
+            url: `https://swapi.co/api/${resource}/2/`
+        })
+        .then(function(response) {
+            self.starWarsResource.list = response.data
+            // $http.get(self.starWarsPerson.films[0]).then(function (response) { //This is a shortcut method of the above. 
+            //     console.log(response);
+            //     self.starWarsMovie = response
+            // })
+            
+        })        
+    }
 
-
-
-
+	self.getResources = function (){
+		$http({
+			method: 'GET',
+			url: 'https://swapi.co/api/'
+			}).then(function(response) {
+                self.resourceArray.list = response.data;
+			})
+    };
+    
+    self.getResources()
 /* POST REQUESTS */
 
 
